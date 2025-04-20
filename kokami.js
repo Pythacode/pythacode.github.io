ad184876d8572922664acb95c2d402572a30a94c
Nathanaëlle <94068090+Pythacode@users.noreply.github.com>
2025-04-20 15:23:57 +0200

    Update kokami.js

--- kokami.js
+++ kokami.js
@@ -1,13 +1,3 @@
-let height = 50;
-let width = 50;
-const kokami = document.getElementById('kokami');
-
-document.addEventListener("mousemove", (event) => {
-    kokami.style.maskImage = kokami.style.webkitMaskImage = 
-                `linear-gradient(transparent, transparent),
-                 radial-gradient(circle at ${event.clientX + width / 2}px ${event.clientY + height / 2}px, transparent 200px, black 250px)`;
-});
-
 let touches = []; // Liste pour stocker les touches
 
 document.addEventListener('keydown', (event) => {
@@ -21,6 +11,6 @@ document.addEventListener('keydown', (event) => {
 
     if (touches.join(", ") == "ArrowUp, ArrowUp, ArrowDown, ArrowDown, ArrowRight, ArrowLeft, ArrowRight, ArrowLeft, a, b" ) {
         console.log("kokami !");
-        kokami.style.display = "block";
+        window.location.href = "/kokami";
     }
 });
