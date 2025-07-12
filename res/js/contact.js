708eba2a566b6655c0574489e86e6b4774c21eea
Nathanaëlle <titouan.petit-frulio@laposte.net>
2025-07-13 01:04:27 +0200

    Add a contact page

---
+++ res/js/contact.js
@@ -0,0 +1,111 @@
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+  ['link'],
+
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
+const quill = new Quill('#message', {
+  modules: {
+    toolbar: toolbarOptions
+  },
+  theme: 'snow'
+});
+
+quill.setContents([])
+
+
+const message = quill.root.innerHTML;
+
+const date = formatDate(new Date());
+
+console.log("Message (HTML) :", message);
+
+const form = document.getElementById('contactForm');
+emailjs.init('kQntJTIis5HL5JgnX');
+
+function formatDate(date) {
+  const joursMois = [
+    'janvier', 'février', 'mars', 'avril', 'mai', 'juin',
+    'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
+  ];
+
+  const jour = date.getDate();
+  const mois = joursMois[date.getMonth()];
+  const annee = date.getFullYear();
+
+  let heures = date.getHours();
+  let minutes = date.getMinutes();
+
+  // Ajouter un zéro devant les minutes si < 10
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+
+  return `${jour} ${mois} ${annee} ${heures}h${minutes}`;
+}
+
+const SubmitButton = document.getElementById('submit');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    SubmitButton.value = "Chargement..."
+        SubmitButton.style = "cursor:default;"
+    SubmitButton.disabled = true;
+
+    const email = document.getElementById('mail').value;
+    const subject = document.getElementById('subject').value;
+    const name = document.getElementById('name').value;
+    const message = quill.root.innerHTML;
+
+    if (message == "<p><br></p>") {
+        alert('Entrez un message')
+        SubmitButton.value = "Valider"
+        SubmitButton.style = ""
+        SubmitButton.disabled = false;
+        return
+    }
+
+    const date = formatDate(new Date());
+
+    console.log("E-mail :", email);
+    console.log("Sujet :", subject);
+    console.log("Message (HTML) :", message);
+
+    const templateParams = {
+            title: subject,
+            name: name,
+            time: date,
+            message: message,
+            mail: email,
+        };
+
+    emailjs.send('service_msk0cz5', 'template_nvzblfj', templateParams)
+    .then(function(response) {
+        window.location.href += '/confirm';
+    }, function(error) {
+        console.error('Erreur envoi email:', error);
+        alert('Erreur lors de l’envoi, réessaiez plus tard.\nErreur : ' + error);
+        form.reset();
+        quill.setContents([]);
+        SubmitButton.value = "Valider"
+        SubmitButton.style = ""
+        SubmitButton.disabled = false;
+
+    });
+
+    // Empêcher le formulaire de s’envoyer (rechargement)
+    return false;
+});
--- res/js/maj.js
+++ res/js/maj.js
@@ -1,25 +1,3 @@
-const nav = document.querySelector("nav");
-nav.addEventListener("click", () => scrollDown(1));
-window.addEventListener("scroll", updateButtonVisibility);
-updateButtonVisibility();
-
-function scrollDown(i) {
-    window.scrollTo({
-        top: window.innerHeight*i,
-        behavior: 'smooth'
-    });
-}
-
-function updateButtonVisibility(i) {
-    if (window.scrollY > 0) {
-        nav.style.opacity = "0";
-        nav.style.pointerEvents = "None";
-    } else {
-        nav.style.opacity = "1";
-        nav.style.pointerEvents = "auto";
-    }
-}
-
 const MAJchamp = document.getElementById('MAJ')
 
 function updtadeMaj(value) {
